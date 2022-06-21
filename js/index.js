59e98adca38b1df8ce5d1f7349a6fd1f6530d2b0
Efezino Idisi <enduranceidisi13@gmail.com>
2022-06-21 20:42:21 +0100

    solution to task

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let fullName = "Idisi Efezino Endurance";
+let height = 165;
+let country = "Nigeria";
+
+console.log("My name is", fullName);
+console.log("I'm " + height + "cm tall");
+console.log("and am from", country);
