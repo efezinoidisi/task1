fcfc34216320bd00b587ce9a79d18959bef949eb
Efezino Idisi <enduranceidisi13@gmail.com>
2022-06-22 14:37:54 +0100

    work on task

--- js/index.js
+++
@@ -1,7 +0,0 @@
-let fullName = "Idisi Efezino Endurance";
-let height = 165;
-let country = "Nigeria";
-
-console.log("My name is", fullName);
-console.log("I'm " + height + "cm tall");
-console.log("and am from", country);
---
+++ js/script.js
@@ -0,0 +1,8 @@
+let fullName = "Idisi Efezino Endurance";
+let height = 165;
+let country = "Nigeria";
+
+// console.log("My name is", fullName);
+// console.log("I'm " + height + "cm tall");
+// console.log("and am from", country);
+alert(`My name is ${fullName} and I am ${height}cm tall. I am from ${country}`);
